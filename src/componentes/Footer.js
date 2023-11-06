076cad85d8b84e4646a0ce76ed6cb556a1d6713e
Leandro Ancalipe <leandroancalipe433@gmail.com>
2023-11-06 12:03:23 -0300

    claser23

---
+++ src/componentes/Footer.js
@@ -0,0 +1,9 @@
+const Footer = (props) => {
+    return (
+        <footer>
+        <p>Diseñado por Leandro Ancalipe </p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/componentes/Header.js
@@ -0,0 +1,11 @@
+// import Nav from "./Nav";
+
+const Header = (props) => {
+    return (
+       <header>
+        <h1>Trabajamos con React</h1>
+       </header>
+    )
+}
+
+export default Header;
